8353566a25def4532b924037e0dc9f9ba790d783
JacksonBowe <jackson.bowe@powertec.com.au>
2022-09-26 17:02:13 +1000

    canvas added, not working

--- src/boot/vis.js
+++ src/boot/vis.js
@@ -2,8 +2,15 @@ import { boot } from 'quasar/wrappers'
 import * as network from "vis-network"
 import * as data from "vis-data"
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+console.log('data', data)
+
+const vis = Object.assign({}, network, data)
+
+
 export default boot(async (/* { app, router, ... } */) => {
-	console.log(Object.assign({}, network, data))
+	console.log('vis', vis)
+	return vis
 })
+
+export { vis }
+
--- src/pages/GraphPage.vue
+++ src/pages/GraphPage.vue
@@ -1,13 +1,17 @@
 <template>
 	<q-page padding class="row" dark>
-		<div>
-			ytuyi
+		<div ref="container">
+			<!-- <div> -->
+				<canvas width="600" height="600"></canvas>
+			<!-- </div> -->
 		</div>
 	</q-page>
 </template>
 
 <script>
 import { useQuasar } from 'quasar';
+import { vis } from 'src/boot/vis';
+import { onMounted, ref } from 'vue';
 // import * as vis from "vis-network"
 // import * as data from "vis-data"
 
@@ -16,7 +20,33 @@ export default {
 	setup () {
 		const q = useQuasar()
 		q.dark.set(true)
-		// console.log(data)
+
+		console.log('vis2', vis)
+		var nodes = new vis.DataSet([
+			{ id: 1, label: "Node 1" },
+			{ id: 2, label: "Node 2" }
+		])
+
+		var edges = new vis.DataSet([
+			{ from: 1, to: 2 }
+		])
+
+		const container = ref(null)
+		var data = {
+			nodes: nodes,
+			edges: edges
+		}
+		onMounted(() => {
+			console.log(container.value)
+			// var network = new vis.Network(container.value, data, {})
+		})
+		//
+		console.log(data)
+
+
+		return {
+			container
+		}
 	}
 }
 </script>
